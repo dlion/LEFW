d49de3c6f71ec7b2b3d93fb930cf6fb8fafcd654
dlion <domenico.luciani@email.it>
2013-04-01 15:46:45 +0200

    Comment.

--- core/config.php
+++ core/config.php
@@ -1,11 +1,8 @@
 <?php
-//Include classes
 include_once('core.class.php');
-//Istance of configuration
+
 $conf = Config::getIstanza('host','db name','user','password','admin name');
-//Istance of PDO
 $db = Db::getIstanza($conf);
-//Retrieve pdo
 $db = $db->getPDO();
 ?>
 
