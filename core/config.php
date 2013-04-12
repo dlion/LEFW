57b6526ba2e8d3354a904b0afd054db4f73cff8b
dlion <domenico.luciani@email.it>
2013-04-12 02:12:29 +0200

    Improved configuration

--- core/config.php
+++ core/config.php
@@ -1,8 +1,24 @@
 <?php
-include_once('core.class.php');
+//Config Class
+include_once('config.class.php');
+//DB Class
+include_once('db.class.php');
 
+/*
+ * -- Configuration --
+ * - @host - Host where is the database
+ * - @database - Name of database
+ * - @user - User to use database
+ * - @password - Password to use database
+ * - @admin - Name or nick of admin
+ */
 $conf = Config::getIstanza('host','db name','user','password','admin name');
+
+// Istances
 $db = Db::getIstanza($conf);
-$db = $db->getPDO();
+if($db != false)
+    $db = $db->getPDO();
+else
+    //La connessione è fallita
 ?>
 
