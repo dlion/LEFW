1635631c65e4096fbfa9deb80d50d439173ca3af
dlion <domenico.luciani@email.it>
2013-04-12 02:13:26 +0200

    Create class file to use db connection class

---
+++ core/db.class.php
@@ -0,0 +1,30 @@
+<?php
+//Classe per la connessione al DB
+class Db {
+    private $db;
+    //Istanza
+    private static $istanza = null;
+    
+    public static function getIstanza($dato)
+    {
+      if(self::$istanza == null)
+        self::$istanza = new self($dato);
+      
+      return self::$istanza;
+    }
+    
+    private function __construct($dato) {
+        try {
+            $this->db = new PDO("mysql:host=".$dato->getHost().";dbname=".$dato->getDbName(), $dato->getDbUser(), $dato->getDbPass());
+        }
+        catch(Exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+    
+    public function getPDO() {
+        return $this->db;
+    }
+}
+?>
