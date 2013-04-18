4bd40c4cdac41c753373c4b6c7f723eeb36fdd87
dlion <domenico.luciani@email.it>
2013-04-18 14:38:45 +0200

    Added methods to show public links and private links

--- core/link.class.php
+++ core/link.class.php
@@ -19,8 +19,9 @@ class Link {
         $this->pdo = $pdo;
     }
     
-    public function getLinkByCategory($category) {
-        $query = $this->pdo->prepare("SELECT id,name,url,category FROM link_link WHERE user = :id AND category = :cate");
+    //Return public links by a category and user
+    public function getPublicLinkByCategory($category) {
+        $query = $this->pdo->prepare("SELECT id,name,url,category FROM link_link WHERE user = :id AND category = :cate AND priv8 = '0'");
         $query->execute(array(':id' => $this->id, ':cate' => $category));
         if($query->rowCount() > 0) {
             $ris = $query->fetchAll();
@@ -30,8 +31,21 @@ class Link {
             return false;
     }
     
-    public function getAllLink() {
-        $query = $this->pdo->prepare("SELECT id,name,url,category FROM link_link WHERE user = :id");
+    //Return private links by a category and user
+    public function getPrivateLinkByCategory($category) {
+        $query = $this->pdo->prepare("SELECT id,name,url,category FROM link_link WHERE user = :id AND category = :cate AND priv8 = '1'");
+        $query->execute(array(':id' => $this->id, ':cate' => $category));
+        if($query->rowCount() > 0) {
+            $ris = $query->fetchAll();
+            return $ris;
+        }
+        else
+            return false;
+    }
+    
+    //Return all public links of an user
+    public function getAllPublicLink() {
+        $query = $this->pdo->prepare("SELECT id,name,url,category FROM link_link WHERE user = :id AND priv8 = '0'");
         $query->execute(array(':id' => $this->id));
         if($query->rowCount() > 0) {
             $ris = $query->fetchAll();
@@ -41,6 +55,19 @@ class Link {
             return false;
     }
     
+    //Return all private links of an user
+    public function getAllPrivateLink() {
+        $query = $this->pdo->prepare("SELECT id,name,url,category FROM link_link WHERE user = :id AND priv8 = '1'");
+        $query->execute(array(':id' => $this->id));
+        if($query->rowCount() > 0) {
+            $ris = $query->fetchAll();
+            return $ris;
+        }
+        else
+            return false;
+    }
+    
+    //Insert a link with category
     public function insertLink($name,$url,$category) {
         
         $query = $this->pdo->prepare("INSERT INTO link_link(name,url,user,category) VALUES(:name,:url,:user,:category)");
