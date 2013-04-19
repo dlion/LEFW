6b348bccd83873043dd87484a86382ca8f091e82
dlion <domenico.luciani@email.it>
2013-04-19 03:19:32 +0200

    Added checkLink metod to verify if link is a true link. Parsed html variables so fixing some vulnerabilities. Added possibility to add private links

--- core/link.class.php
+++ core/link.class.php
@@ -67,18 +67,46 @@ class Link {
             return false;
     }
     
+    public function checkURL($url) {
+        $reg = '%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu';
+        
+        $url = htmlspecialchars(trim($url));
+        
+        if(preg_match($reg,$url))
+            return true;
+        else
+        {
+            $url = "http://".$url;
+            if(preg_match($reg,$url))
+                return true;
+            else
+                return false;
+        }
+    }
+    
     //Insert a link with category
-    public function insertLink($name,$url,$category) {
+    public function insertLink($name,$url,$category,$priv) {
         
-        $query = $this->pdo->prepare("INSERT INTO link_link(name,url,user,category) VALUES(:name,:url,:user,:category)");
+        if($this->checkURL($url) === true)
+        {
+            $url = htmlspecialchars(trim($url));
+            $name = htmlspecialchars(trim($name));
+            $category = htmlspecialchars(trim($category));
+            $priv = htmlspecialchars(trim($priv));
         
-        $ris = $query->execute(array(':name' => $name,
-                                     ':url' => $url,
-                                     ':user' => $this->id,
-                                     ':category' => $category
-                                    )
-                                );
-        return $ris;
+            $query = $this->pdo->prepare("INSERT INTO link_link(name,url,user,priv8,category) VALUES(:name,:url,:user,:priv,:category)");
+        
+            $ris = $query->execute(array(':name' => $name,
+                                        ':url' => $url,
+                                        ':user' => $this->id,
+                                        ':priv' => $priv,
+                                        ':category' => $category
+                                        )
+                                    );
+            return $ris;
+        }
+        else
+            return false;
     }
 }
 ?>
